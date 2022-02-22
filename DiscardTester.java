6cd33543dd0d1c4becf164374c9dbc1c4e7133c4
Maryamkusman <93306676+Maryamkusman@users.noreply.github.com>
2022-02-22 13:02:59 -0800

    libs +starter

---
+++ DiscardTester.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class DiscardTester {
+    @Test
+    public void addition() {
+        assertEquals(2, Discard.add(1, 3));
+    }
+    
+}
