85467327687b865e79294f6d45d6edd315b2adc7
Maryamkusman <93306676+Maryamkusman@users.noreply.github.com>
2022-02-22 13:08:26 -0800

    Update DiscardTester.java

--- DiscardTester.java
+++ DiscardTester.java
@@ -3,7 +3,7 @@ import org.junit.*;
 public class DiscardTester {
     @Test
     public void addition() {
-        assertEquals(2, Discard.add(1, 3));
+        assertEquals(2, Discard.add(1, 1));
     }
     
 }
